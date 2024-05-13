7536a05485effea52d1393cdce449c7da7fe658f
xpero39 <peter.vreca@gmail.com>
2024-05-13 17:19:20 +0200

    Added GTIN to Product and functions to get/create/update/delete

--- src/models/product.js
+++ src/models/product.js
@@ -2,7 +2,45 @@ require('dotenv').config()
 const { Sequelize, DataTypes, Model } = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_URL);
 
-class Product extends Model {}
+class Product extends Model {
+    
+    static async findAllProducts() {
+        return Product.findAll();
+    }
+
+    static async findProductById(id) {
+        return Product.findByPk(id);
+    }
+
+    static async createProduct(product) {
+        return Product.create(product);
+    }
+
+    static async updateProductById(id, product) {
+        return Product.update(product, {
+            where: {
+                id: id
+            }
+        });
+    }
+
+    static async deleteProductById(id) {
+        return Product.destroy({
+            where: {
+                id: id
+            }
+        });
+    }
+
+    static async getProductByGTIN(GTIN) {
+        return Product.findOne({
+            where: {
+                GTIN: GTIN
+            }
+        });
+    }
+
+}
 
 Product.init({
     id: {
@@ -20,6 +58,10 @@ Product.init({
         defaultValue: 0.0
     },
     description: DataTypes.STRING,
+    GTIN: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+    },
     createdAt: DataTypes.DATE,
     updatedAt: DataTypes.DATE,
 
